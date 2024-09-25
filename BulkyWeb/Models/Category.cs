625b122f27a8c97c65318a809f0761361fdb820b
apple <apple@appurunoMacBook-Pro.local>
2024-09-25 14:59:17 +0530

    adding the category model

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations; // we need to add this extension of key
+
+
+namespace BulkyWeb.Models
+{
+	public class Category
+	{
+        //[Key]   it will definied the primary key 
+	    public int Id { get; set; }
+
+        //[Required] name is required 
+        public string Name { get; set; }
+        public int DisplayOrder { get; set;
+        }
+     
+
+    }
+}
+
