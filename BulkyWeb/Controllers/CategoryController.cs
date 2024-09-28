c400502c08f0fcd3599aa6297338fa9dee8617b3
apple <apple@appurunoMacBook-Pro.local>
2024-09-28 15:12:37 +0530

    CRUD Perform

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -26,18 +26,20 @@ namespace BulkyWeb.Controllers
             return View(objCategoryList);
         }
 
+
         public IActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if(obj.Name == obj.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("Name", "The displayorder canot exactly match the Name");
             }
-            if (obj.Name.ToLower() == "test")
+            if (obj.Name != null && obj.Name.ToLower() == "test")
             {
                 ModelState.AddModelError("", "Test is an Invalid value");
             }
@@ -53,6 +55,86 @@ namespace BulkyWeb.Controllers
             }
             return View();
             
+        }
+
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null  || id == 0)
+            {
+                return NotFound();
+            }
+            // three methods to find the id 
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u =>u.Id ==id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (categoryFromDb== null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+          
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                //return View();  // because it will go to create view
+
+                return RedirectToAction("Index"); // we need to go for index after creating so we used redirectToAction method
+
+            }
+            return View();
+
+        }
+
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index"); // we need to go for index after creating so we used redirectToAction method
+
+            }
+
+
+            return View();
+
+
         }
     }
 }
